e281c9ab54a6e9a4ea4fd3e7c0b52b1ee6b9ba10
joshhunt <josh@trtr.co>
2025-02-18 19:41:38 +0000

    log env vars

--- grafana-alloy/alloy-wrapper.sh
+++ grafana-alloy/alloy-wrapper.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 source "/opt/homebrew/etc/alloy/config.env"
 
+echo "ALLOY_LOKI_URL: $ALLOY_LOKI_URL"
+echo "ALLOY_LOKI_USER: $ALLOY_LOKI_USER"
+echo "ALLOY_PROM_URL: $ALLOY_PROM_URL"
+echo "ALLOY_PROM_USER: $ALLOY_PROM_USER"
+
 /opt/homebrew/opt/alloy/bin/alloy \
     run /Users/josh/selfhost-stack/grafana-alloy/config \
     --server.http.listen-addr=0.0.0.0:12345 \
